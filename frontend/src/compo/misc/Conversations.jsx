04181bf5427d19ea54b3356479db2cfba138575c
OmDhavale <om.17933@sakec.ac.in>
2025-02-27 23:41:50 +0530

    deploy now

--- frontend/src/compo/misc/Conversations.jsx
+++ frontend/src/compo/misc/Conversations.jsx
@@ -33,6 +33,8 @@ const Conversations = () => {
         //     Loading Users...
         //   </div>
         // </div>
+        // 4 divs for user loading state
+        <div> 
         <div class="flex animate-pulse space-x-4">
           <div class="size-10 rounded-full bg-gray-200"></div>
           <div class="flex-1 space-y-6 py-1">
@@ -45,6 +47,46 @@ const Conversations = () => {
               <div class="h-2 rounded bg-gray-200"></div>
             </div>
           </div>
+        </div>
+        <div class="flex animate-pulse space-x-4">
+          <div class="size-10 rounded-full bg-gray-200"></div>
+          <div class="flex-1 space-y-6 py-1">
+            <div class="h-2 rounded bg-gray-200"></div>
+            <div class="space-y-3">
+              <div class="grid grid-cols-3 gap-4">
+                <div class="col-span-2 h-2 rounded bg-gray-200"></div>
+                <div class="col-span-1 h-2 rounded bg-gray-200"></div>
+              </div>
+              <div class="h-2 rounded bg-gray-200"></div>
+            </div>
+          </div>
+        </div>
+        <div class="flex animate-pulse space-x-4">
+          <div class="size-10 rounded-full bg-gray-200"></div>
+          <div class="flex-1 space-y-6 py-1">
+            <div class="h-2 rounded bg-gray-200"></div>
+            <div class="space-y-3">
+              <div class="grid grid-cols-3 gap-4">
+                <div class="col-span-2 h-2 rounded bg-gray-200"></div>
+                <div class="col-span-1 h-2 rounded bg-gray-200"></div>
+              </div>
+              <div class="h-2 rounded bg-gray-200"></div>
+            </div>
+          </div>
+        </div>
+        <div class="flex animate-pulse space-x-4">
+          <div class="size-10 rounded-full bg-gray-200"></div>
+          <div class="flex-1 space-y-6 py-1">
+            <div class="h-2 rounded bg-gray-200"></div>
+            <div class="space-y-3">
+              <div class="grid grid-cols-3 gap-4">
+                <div class="col-span-2 h-2 rounded bg-gray-200"></div>
+                <div class="col-span-1 h-2 rounded bg-gray-200"></div>
+              </div>
+              <div class="h-2 rounded bg-gray-200"></div>
+            </div>
+          </div>
+        </div>
         </div>
       ) : (
         <></>
